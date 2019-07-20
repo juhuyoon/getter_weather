3b50b95e13c22ef93a56e7a6ece33f652a36e908
n00b-c0der <twh710twh@gmail.comm>
2019-07-20 13:10:53 -0400

    added working routes to server

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,11 +1,6 @@
-var mongoose = require("mongoose");
-const express = require("express");
+const express = require('express')
 const axios = require('axios');
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Server is working!")
-});
+var app = express()
 
 app.get('/apixu', function (req, res) {
     
@@ -54,37 +49,3 @@ app.get('/openweather', function (req, res) {
         });
     res.send("DarkSky is working");
   });
-
-
-app.listen(3000, () => {
-    console.log("Listening on localhost:3000")
-});
-
-mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true});
-
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-    console.log("Connected!")
-});
-
-var testSchema = new mongoose.Schema({
-    city: String,
-    zip: String,
-    weather: String
-});
-
-testSchema.methods.speak = function () {
-    var dataSpew = (this.city, this.zip, this.weather);
-    console.log(dataSpew);
-};
-
-app.get("/weather", (req, res) => {
-    console.log(testSchema);
-})
-
-var test = mongoose.model("test", testSchema);
-
-var Atlanta = new test({ city: 'Atlanta', zip: '30009', weather: 'cloudy'});
-
-Atlanta.speak();
