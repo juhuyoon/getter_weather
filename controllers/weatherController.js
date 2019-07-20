52e26ec30596ebd8a670270507def02260749d57
n00b-c0der <twh710twh@gmail.comm>
2019-07-20 12:14:10 -0400

    added API calls

--- controllers/weatherController.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.get('/weather', function (req, res) {
-  res.send("this is working")
-  $.getJSON('http://api.apixu.com/v1/current.json?key=9a6d1cab9e4a4f8f8d4230629191807&q=Paris', function(result) {
-      console.log(result)
-  });
-})
