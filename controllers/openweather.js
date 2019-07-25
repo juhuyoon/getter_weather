0924736c62816af04dd00ad232428d94820782cc
n00b-c0der <twh710twh@gmail.comm>
2019-07-25 15:36:20 -0400

    API call into JSON

--- controllers/openweather.js
+++ controllers/openweather.js
@@ -9,5 +9,10 @@ request(url, function (err, response, body) {
     console.log('error:', error);
   } else {
     console.log('body:', body);
+    var weather = JSON.parse(body);
+    console.log(weather);
+    var temp = `It's ${weather.main.temp} degrees in
+    ${weather.name}!`;
+    console.log(temp);
   }
 });
