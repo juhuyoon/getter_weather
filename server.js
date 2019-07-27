5fdd69d6c74bcaf779ea0253b684249198926f1b
n00b-c0der <twh710twh@gmail.comm>
2019-07-27 10:26:40 -0400

    push

--- server.js
+++ server.js
@@ -2,64 +2,20 @@ var mongoose = require("mongoose");
 const express = require("express");
 const axios = require('axios');
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Server is working!")
-});
-
-app.get('/apixu', function (req, res) {
-    
-    axios({
-        method: 'get',
-        url: 'http://api.apixu.com/v1/current.json?key=9a6d1cab9e4a4f8f8d4230629191807&q=Paris',
-        })
-        .then(function(response) {
-            console.log(response);
-        });
-    res.send("APIXU is working");
-  });
-
-app.get('/openweather', function (req, res) {
-    
-    axios({
-        method: 'get',
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=0df9f64365060ae81c16eb4855a81df7',
-        })
-        .then(function(response) {
-            console.log(response);
-        });
-    res.send("Openweather is working");
-  });
-
-  app.get('/accuweather', function (req, res) {
-    
-    axios({
-        method: 'get',
-        url: 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/348181?apikey=aIM0XKfnNBCyI8Ya7Q5Shb1RYTpCL3Od',
-        })
-        .then(function(response) {
-            console.log(response);
-        });
-    res.send("Accuweather is working");
-  });
-
-  app.get('/darksky', function (req, res) {
-    
-    axios({
-        method: 'get',
-        url: 'https://api.darksky.net/forecast/112b5fa6d162582af407458fecc3d47d/37.8267,-122.4233',
-        })
-        .then(function(response) {
-            console.log(response);
-        });
-    res.send("DarkSky is working");
-  });
-
+var routes = require('./controllers/weatherController');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log("Listening on localhost:3000")
 });
 
+app.get("/", (req, res) => {
+    res.send("Server is working!")
+});
+
+   
 mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true});
 
 var db = mongoose.connection;
@@ -79,12 +35,20 @@ testSchema.methods.speak = function () {
     console.log(dataSpew);
 };
 
-app.get("/weather", (req, res) => {
-    console.log(testSchema);
-})
-
 var test = mongoose.model("test", testSchema);
 
 var Atlanta = new test({ city: 'Atlanta', zip: '30009', weather: 'cloudy'});
 
 Atlanta.speak();
+
+app.post("/addname", (req, res) => {
+    var myData = new test(req.body);
+    console.log(myData);
+    myData.save()
+    .then(item => {
+    res.send("item saved to database");
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+});
