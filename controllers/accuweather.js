94318b1da89becaae4a94b053964311ecfc2a1de
OrionHolcomb <orionholcomb@gmail.com>
2019-07-30 20:51:39 -0400

    3 working schemas for 3 seperate APIs and half a schema for one of them

--- controllers/accuweather.js
+++ controllers/accuweather.js
@@ -2,6 +2,7 @@ let request = require('request');
 var mongoose = require("mongoose");
 
 let apiKey = '0df9f64365060ae81c16eb4855a81df7';
+var userCity = "Atlanta";
 let city = '348181';
 let url = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${city}?apikey=aIM0XKfnNBCyI8Ya7Q5Shb1RYTpCL3Od`
 
@@ -9,8 +10,19 @@ request(url, function (err, body) {
   if(err){
     console.log('error:', error);
   } else {
-    var weather = JSON.parse(body);
-    console.log(weather);
+    var bodyParse = JSON.parse(body.body);
+    console.log(bodyParse);
+
+    var weather = bodyParse.DailyForecasts[0]
+
+    console.log()
+
+    var weatherObj = {
+      city: userCity,
+      condition: weather.Day.IconPhrase,
+      temp: "",
+      humidity: ""
+    };
 
     }
   }
