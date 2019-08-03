32b5c9aab29a6d37709ffe9e21db3e23b169589e
OrionHolcomb <orionholcomb@gmail.com>
2019-08-03 13:07:57 -0400

    created the data controller to handle all database requests

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -11,5 +11,21 @@ module.exports = {
         db.forecast.findById(req.params.id)
         .then(dbForecast => res.json(dbForecast))
         .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        db.forecast.create(req.body)
+        .then(dbForecast => res.json(dbForecast))
+        .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        db.forecast.findOneAndUpdate({ id: req.params.id }, req.body)
+        .then(dbForecast => res.json(dbForecast))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.forecast.findById(req.params.id)
+        .then(dbForecast => dbForecast.remove())
+        .then(dbForecast => res.json(dbForecast))
+        .catch(err => res.status(422).json(err));
     }
-}
+};
