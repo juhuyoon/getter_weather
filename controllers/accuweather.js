0ad130af0a402b1b463b38d44a7639e77cb1574a
n00b-c0der <twh710twh@gmail.comm>
2019-08-06 21:37:14 -0400

    pushing everything I have. My sphaghetti, my code, my struggles, my tears, and my fears

--- controllers/accuweather.js
+++ controllers/accuweather.js
@@ -3,23 +3,16 @@ var mongoose = require("mongoose");
 
 let city = '348181';
 let url = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${city}?apikey=aIM0XKfnNBCyI8Ya7Q5Shb1RYTpCL3Od`
-
+request(url, function(err, body) {
   if(err){
-    console.log('error:', error);
+    console.log('error:');
   } else {
     var bodyParse = JSON.parse(body.body);
     console.log(bodyParse);
 
     var weather = bodyParse.DailyForecasts[0]
 
-    console.log()
+    console.log(weather)
 
-    var weatherObj = {
-      city: userCity,
-      condition: weather.Day.IconPhrase,
-      temp: "",
-      humidity: ""
-    };
-
-}
-
+  };
+})
